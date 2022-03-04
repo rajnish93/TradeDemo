de3aac92aae3e0fbf070f5ffea527a55271a7690
Rajnish Singh <10815566+rajnish93@users.noreply.github.com>
2022-03-04 18:28:24 +0530

    Modify Application Directory Structure

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { Block, GalioProvider } from "galio-framework";
 import React from "react";
-import customTheme from "./components/customTheme";
-import Onboarding from "./screens/Onboarding/Onboarding";
+import customTheme from "./src/components/customTheme";
+import Onboarding from "./src/screens/Onboarding/Onboarding";
 
 export default function App() {
   return (
