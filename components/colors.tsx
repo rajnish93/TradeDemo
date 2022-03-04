2cb74faabf38c9634a93b002caf43dd93c44bc4d
Rajnish Singh <10815566+rajnish93@users.noreply.github.com>
2022-03-04 12:56:46 +0530

    Added Colors and Common Component

---
+++ components/colors.tsx
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: "#ef835d",
+  secondary: "#2c365a",
+  tertiary: "#85c6d8",
+  accent: "#fbcd77",
+  white: "#fff",
+  grayExtra: "#9CA3AF",
+  gray: "#D1D5DB",
+  graylight: "#F3F4F6",
+  graydark: "#4B5563",
+  primaryLight: "rgba(239, 131, 93, 1)",
+  tertiaryLight: "rgba(133, 198, 216, 0.2)",
+  accentLight: "rgba(251, 205, 119, 0.3)",
+};
